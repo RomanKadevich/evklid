cc113507b44fed4a8135f338519b6455767a9059
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2023-02-27 22:24:39 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded',function(){
-   document.querySelector('#burger').addEventListener('click',function(){
-       document.querySelector('#menu').classList.toggle('menu-active')
-})
-// document.querySelector('#body').addEventListener('click',function(){
-//     document.querySelector('#menu').classList.remove('menu-active')
-// })
-})
